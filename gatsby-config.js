333ceb2202fca8774f6bdd942f8ede52ee36b08d
Alessandro Burato <alessandro.matteo.burato@gmail.com>
2019-06-11 16:46:31 +0200

    bump deps and remove useless feed and analytics plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,13 +58,13 @@ module.exports = {
         useMozJpeg: true
       }
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      }
-    },
-    `gatsby-plugin-feed`,
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     //trackingId: `ADD YOUR TRACKING ID HERE`,
+    //   }
+    // },
+    // `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
