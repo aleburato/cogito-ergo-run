6a27cc7568d951d6aa4674198bbdc805c2ac462e
Alessandro Burato <alessandro.burato@outlook.com>
2019-06-19 08:24:28 +0200

    Turn class components to simple functions

--- src/components/header.js
+++ src/components/header.js
@@ -6,51 +6,47 @@ import React from "react";
 import logo from "../../content/assets/running.svg";
 import { rhythm, scale } from "../utils/typography";
 
-class Header extends React.Component {
-  render() {
-    const { isMain, title, description } = this.props;
-
-    const header = isMain ? (
-      <div
+function Header({ isMain, title, description }) {
+  const header = isMain ? (
+    <div
+      style={{
+        marginBottom: rhythm(1.5)
+      }}
+    >
+      <h1
         style={{
-          marginBottom: rhythm(1.5)
+          ...scale(1.2),
+          marginTop: 0,
+          marginBottom: 0,
+          marginLeft: "-2px"
         }}
       >
-        <h1
-          style={{
-            ...scale(1.2),
-            marginTop: 0,
-            marginBottom: 0,
-            marginLeft: "-2px"
-          }}
-        >
-          <Link to={`/`}>{title}</Link>
-          <img
-            alt="The site logo"
-            style={{ width: "24px", height: "24px", margin: "0 0 0 -0.1em" }}
-            src={logo}
-          />
-        </h1>
-        <p>
-          <em>{description}.</em>
-        </p>
-      </div>
-    ) : (
-      <div>
-        <h3
-          style={{
-            marginTop: 0,
-            marginBottom: "0.25em"
-          }}
-        >
-          <Link to={`/`}>{title}</Link>
-        </h3>
-        <p>...</p>
-      </div>
-    );
+        <Link to={`/`}>{title}</Link>
+        <img
+          alt="The site logo"
+          style={{ width: "24px", height: "24px", margin: "0 0 0 -0.1em" }}
+          src={logo}
+        />
+      </h1>
+      <p>
+        <em>{description}.</em>
+      </p>
+    </div>
+  ) : (
+    <div>
+      <h3
+        style={{
+          marginTop: 0,
+          marginBottom: "0.25em"
+        }}
+      >
+        <Link to={`/`}>{title}</Link>
+      </h3>
+      <p>...</p>
+    </div>
+  );
 
-    return <header>{header}</header>;
-  }
+  return <header>{header}</header>;
 }
 
 export default Header;
--- src/components/layout.js
+++ src/components/layout.js
@@ -4,40 +4,33 @@ import Header from "./header";
 import React from "react";
 import { rhythm } from "../utils/typography";
 
-class Layout extends React.Component {
-  render() {
-    const { location, title, description, children } = this.props;
-    const rootPath = `${__PATH_PREFIX__}/`;
+function Layout({ location, title, description, children }) {
+  const rootPath = `${__PATH_PREFIX__}/`;
 
-    return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(28),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`
-        }}
-      >
-        <Header
-          isMain={location.pathname === rootPath}
-          title={title}
-          description={description}
-        />
-        <main>{children}</main>
-        <footer>
-          <p>...</p>
-          <p>
-            <em>
-              <small>
-                © {new Date().getFullYear()}, costruito con amore, sudore, odore e{" "}
-                <a href="https://www.gatsbyjs.org">gatsbyjs</a>.
-              </small>
-            </em>
-          </p>
-        </footer>
-      </div>
-    );
-  }
+  return (
+    <div
+      style={{
+        marginLeft: `auto`,
+        marginRight: `auto`,
+        maxWidth: rhythm(28),
+        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`
+      }}
+    >
+      <Header isMain={location.pathname === rootPath} title={title} description={description} />
+      <main>{children}</main>
+      <footer>
+        <p>...</p>
+        <p>
+          <em>
+            <small>
+              © {new Date().getFullYear()}, costruito con amore, sudore, odore e{" "}
+              <a href="https://www.gatsbyjs.org">gatsbyjs</a>.
+            </small>
+          </em>
+        </p>
+      </footer>
+    </div>
+  );
 }
 
 export default Layout;
