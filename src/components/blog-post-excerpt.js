6bd465f4ebc2e4fdd29e75c2e8dd00dba2d2c2a1
Alessandro Burato <alessandro.burato@outlook.com>
2019-09-24 09:18:18 +0200

    the whole excerpt is now clickable

--- src/components/blog-post-excerpt.js
+++ src/components/blog-post-excerpt.js
@@ -3,6 +3,8 @@ import { Link } from "gatsby";
 
 import FeaturedImage from "./featured-image";
 
+import "./blog-post-excerpt.css";
+
 export function BlogPostExcerpt({ post }) {
   const {
     frontmatter: { date, title, featuredImage, featuredImageDesc },
@@ -12,24 +14,18 @@ export function BlogPostExcerpt({ post }) {
   const actualTitle = title || slug;
   return (
     <div className="post-block" key={slug}>
-      <h2 className="blog-post-title">
-        <Link to={slug}>{actualTitle}</Link>
-      </h2>
-      <p className="blog-post-date txt-small txt-muted">{date}</p>
       <Link to={slug}>
+        <h2 className="blog-post-title">{actualTitle}</h2>
+        <p className="blog-post-date txt-small txt-muted">{date}</p>
         <FeaturedImage image={featuredImage} imageCaption={featuredImageDesc} />
+        <div
+          className="blog-post-excerpt"
+          dangerouslySetInnerHTML={{ __html: excerpt }}
+        />
+        <p className="txt-small">
+          <em>(leggi tutto)</em>
+        </p>
       </Link>
-      <div
-        className="post-excerpt"
-        dangerouslySetInnerHTML={{ __html: excerpt }}
-      />
-      <p>
-        <small>
-          <em>
-            <Link to={slug}>(leggi tutto)</Link>
-          </em>
-        </small>
-      </p>
     </div>
   );
 }
