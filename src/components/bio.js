6a701dc5959d93ad3e9e39d1bf642aff1e9f31c6
Alessandro Burato <alessandro.burato@outlook.com>
2019-09-25 08:27:21 +0200

    wip

--- src/components/bio.js
+++ src/components/bio.js
@@ -2,7 +2,6 @@ import { StaticQuery, graphql } from "gatsby";
 
 import Image from "gatsby-image";
 import React from "react";
-import { rhythm } from "../utils/typography";
 
 import "./bio.css";
 
@@ -17,23 +16,15 @@ function Bio() {
             <Image
               fixed={data.avatar.childImageSharp.fixed}
               alt={authorName}
-              style={{
-                marginRight: rhythm(1 / 2),
-                marginBottom: 0,
-                minWidth: 80,
-                minHeight: 80,
-                borderRadius: `100%`
-              }}
-              imgStyle={{
-                borderRadius: `50%`
-              }}
+              className="author-avatar"
             />
             <div>
               <p>
-                <strong>{authorName}</strong>. Run-nerd, centauro a strappi, marito,
-                babbo. Sfortunato nel gioco.{" "}
+                <strong>{authorName}</strong>. Run-nerd, centauro a strappi,
+                marito, babbo. Sfortunato nel gioco.{" "}
                 <a href={`https://twitter.com/${social.twitter}`}>twitter</a>,{" "}
-                <a href={`https://facebook.com/${social.facebook}`}>facebook</a> e{" "}
+                <a href={`https://facebook.com/${social.facebook}`}>facebook</a>{" "}
+                e{" "}
                 <a href={`https://www.instagram.com/${social.instagram}`}>
                   #hofattoanchefotobuone
                 </a>
