37a887fa59cd1672b6d699110e993097e332e162
Alessandro Burato <alessandro.burato@outlook.com>
2019-10-01 18:33:29 +0200

    wip

--- src/components/header.js
+++ src/components/header.js
@@ -1,45 +1,27 @@
 import { Link } from "gatsby";
 import React from "react";
 
-import logo from "../../content/assets/running.svg";
-import { rhythm } from "../utils/typography";
+import SiteLogo from "./site-logo";
 
 import "./header.scss";
 
 function Header({ isMain, title, description }) {
-  const header = isMain ? (
-    <div
-      style={{
-        marginBottom: rhythm(1.5)
-      }}
-    >
-      <h1 className="blog-title">
-        <Link to={`/`}>{title}</Link>
-        <img
-          alt="The site logo"
-          style={{ width: "24px", height: "24px", margin: "0 0 0 -0.1em" }}
-          src={logo}
-        />
-      </h1>
-      <p>
-        <em>{description}.</em>
-      </p>
-    </div>
-  ) : (
-    <div>
-      <h3
-        style={{
-          marginTop: 0,
-          marginBottom: "0.25em"
-        }}
-      >
-        <Link to={`/`}>{title}</Link>
-      </h3>
-      <p>...</p>
-    </div>
+  const headerClass = isMain ? "blog-title" : "blog-title-small";
+  return (
+    <header>
+      <div className="header-container">
+        <h1 className={headerClass}>
+          <Link to={`/`}>{title}</Link>
+          {isMain && <SiteLogo />}
+        </h1>
+        {isMain ? (
+          <p className="blog-description">{description}.</p>
+        ) : (
+          <p>...</p>
+        )}
+      </div>
+    </header>
   );
-
-  return <header>{header}</header>;
 }
 
 export default Header;
---
+++ src/components/site-logo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import logo from "../../content/assets/running.svg";
+
+function SiteLogo() {
+  return (
+    <img
+      alt="The site logo"
+      style={{ width: "24px", height: "24px", margin: "0 0 0 -0.1em" }}
+      src={logo}
+    />
+  );
+}
+
+export default SiteLogo;
