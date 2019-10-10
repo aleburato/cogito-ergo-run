c3b5e835b66c8af6aacc0a4acfae8e2d2c3f95bb
Alessandro Burato <alessandro.burato@iongroup.com>
2019-10-10 17:03:32 +0200

    wip

--- src/components/site-logo.js
+++ src/components/site-logo.js
@@ -2,9 +2,12 @@ import React from "react";
 
 import Logo from "../../assets/running.svg";
 
+import "./site-logo.scss";
+
 function SiteLogo() {
   return (
     <Logo
+      className="site-logo"
       alt="The site logo"
       style={{ width: "24px", height: "24px", margin: "0 0 0 -0.1em" }}
     />
